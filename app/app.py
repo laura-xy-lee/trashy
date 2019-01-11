405843457248ca050e0c5602c04fe3e4a248105a
Laura Lee <laura.x.y.lee@gmail.com>
2019-01-11 17:51:30 +0800

    first commit

---
+++ app/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import render_template
+
+# creates a flask application, named app
+app = Flask(__name__)
+
+
+# a route where we will display the main page, via an html template
+@app.route("/")
+def homepage():
+    return render_template('index.html')
+
+
+# run the application
+if __name__ == "__main__":
+    app.run(debug=True)
